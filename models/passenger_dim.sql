4c6214d196501c0020fbb808cbf05714a1c90ca0
ktrung1709 <ktrung1709@gmail.com>
2023-11-17 22:03:33 +0700

    fix passenger dim

--- models/passenger_dim.sql
+++ models/passenger_dim.sql
@@ -2,7 +2,7 @@ with passenger_source as (
     SELECT p.passenger_id, p.passportno, p.firstname, p.lastname, pd.birthdate, pd.sex, pd.street, pd.city, pd.zip, pd.country, pd.emailaddress, pd.telephoneno
     FROM passenger p
     INNER JOIN passengerdetails pd
-    ON p.passenger_id = pd.passenger_id;
+    ON p.passenger_id = pd.passenger_id
 )
 
 select *
