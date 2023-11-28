e3ad7cb81a8006346623b655919904565377e545
ladailam382 <ladailam382@gmail.com>
2023-11-28 22:24:36 +0700

    update

--- macros/date_dim.sql
+++ macros/date_dim.sql
@@ -10,37 +10,37 @@ WITH RECURSIVE RecursiveDates (DateValue) AS (
 
   , 
   dates_fin AS (
-  SELECT d1.date AS Carlendar_Date,
-         EXTRACT(DAYOFWEEK FROM d1.date) as Day_Of_Week,
-         DATE_FORMAT(d1.date, '%a') as Day_Of_Week_Name,
-         DATE_TRUNC(d1.date, 'WEEK') AS Cal_Week_Start_Date, --Monday Start
-         EXTRACT(DAY FROM d1.date) AS Day_Of_Month,
-         EXTRACT(MONTH FROM d1.date) AS Cal_Month,
-         DATE_FORMAT(d1.date, '%M') AS Cal_Mon_Name,
-         DATE_FORMAT(d1.date, '%b') AS Cal_Mon_Name_Short,
-         EXTRACT(quarter FROM d1.date) AS Cal_Quarter,
-         CONCAT ('Q',EXTRACT(quarter FROM d1.date)) AS Cal_Quarter_Name,
-         EXTRACT(year FROM d1.date) AS Cal_Year,
-         CASE EXTRACT(DAYOFWEEK FROM d1.date)
+  SELECT d1.DateValue AS Carlendar_Date,
+         EXTRACT(DAYOFWEEK FROM d1.DateValue) as Day_Of_Week,
+         DATE_FORMAT(d1.DateValue, '%a') as Day_Of_Week_Name,
+         DATE_TRUNC(d1.DateValue, 'WEEK') AS Cal_Week_Start_Date, --Monday Start
+         EXTRACT(DAY FROM d1.DateValue) AS Day_Of_Month,
+         EXTRACT(MONTH FROM d1.DateValue) AS Cal_Month,
+         DATE_FORMAT(d1.DateValue, '%M') AS Cal_Mon_Name,
+         DATE_FORMAT(d1.DateValue, '%b') AS Cal_Mon_Name_Short,
+         EXTRACT(quarter FROM d1.DateValue) AS Cal_Quarter,
+         CONCAT ('Q',EXTRACT(quarter FROM d1.DateValue)) AS Cal_Quarter_Name,
+         EXTRACT(year FROM d1.DateValue) AS Cal_Year,
+         CASE EXTRACT(DAYOFWEEK FROM d1.DateValue)
            WHEN 6 THEN TRUE
            WHEN 7 THEN TRUE
            ELSE FALSE
          END AS Is_Weekend,
-         CASE WHEN EXTRACT(MONTH FROM d1.date) < 7
-           THEN EXTRACT(YEAR FROM d1.date)
-           ELSE EXTRACT(YEAR FROM d1.date) + 1
+         CASE WHEN EXTRACT(MONTH FROM d1.DateValue) < 7
+           THEN EXTRACT(YEAR FROM d1.DateValue)
+           ELSE EXTRACT(YEAR FROM d1.DateValue) + 1
          END AS Fin_Year,
-         CASE WHEN EXTRACT(MONTH FROM d1.date) < 7
-           THEN EXTRACT(MONTH FROM d1.date) + 6
-           ELSE EXTRACT(MONTH FROM d1.date) - 6
+         CASE WHEN EXTRACT(MONTH FROM d1.DateValue) < 7
+           THEN EXTRACT(MONTH FROM d1.DateValue) + 6
+           ELSE EXTRACT(MONTH FROM d1.DateValue) - 6
          END AS Fin_Period,
-         CASE WHEN EXTRACT(MONTH FROM d1.date) < 7
-           THEN EXTRACT(quarter FROM d1.date) + 2
-           ELSE EXTRACT(quarter FROM d1.date) - 2
+         CASE WHEN EXTRACT(MONTH FROM d1.DateValue) < 7
+           THEN EXTRACT(quarter FROM d1.DateValue) + 2
+           ELSE EXTRACT(quarter FROM d1.DateValue) - 2
          END AS Fin_Quarter,
-         CASE WHEN d1.date < date_trunc(d1.date, 'year') + interval '6 months'
-           THEN EXTRACT(WEEK FROM (d1.date - interval '6 months'))::integer
-           ELSE EXTRACT(WEEK FROM (d1.date + interval '6 months'))::integer
+         CASE WHEN d1.DateValue < date_trunc(d1.DateValue, 'year') + interval '6 months'
+           THEN EXTRACT(WEEK FROM (d1.DateValue - interval '6 months'))::integer
+           ELSE EXTRACT(WEEK FROM (d1.DateValue + interval '6 months'))::integer
          END AS Fin_Week
   FROM RecursiveDates d1
 )
