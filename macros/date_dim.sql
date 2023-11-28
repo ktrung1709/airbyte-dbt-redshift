695f817873e401bf6b7f6222b1bb118c97a93618
ladailam382 <75711891+ladailam382@users.noreply.github.com>
2023-11-28 09:34:06 +0700

    Create date_dim.sql

---
+++ macros/date_dim.sql
@@ -0,0 +1,26 @@
+{% macro generate_date_dim() %}
+    with date_series as (
+        select 
+            dateadd(day, number, '2015-01-01') as date
+        from 
+            (select row_number() over (order by (select null)) - 1 as number from sys.all_objects) a
+        where 
+            dateadd(day, number, '2015-01-01') <= getdate()
+    ),
+    date_parts as (
+        select
+            date,
+            datepart(dw, date) as day_of_week,
+            datepart(wk, date) as week_of_month,
+            datepart(mm, date) as month,
+            datepart(qq, date) as quarter,
+            datepart(yy, date) as year
+        from 
+            date_series
+    )
+    insert into date_dim
+    select 
+        *
+    from 
+        date_parts
+{% endmacro %}
