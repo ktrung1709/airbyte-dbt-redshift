467d6533b1d1debe9382bee5ecd71a4fba2a22e9
ladailam382 <ladailam382@gmail.com>
2023-11-29 22:31:01 +0700

    update

--- models/date_time.sql
+++ models/date_time.sql
@@ -1,10 +1,12 @@
 
 -- {{ generate_dates_dimension ("2015-01-01") }}
-with date_spine as ({{-
+with date_spine as (
+    {{
     dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2015-01-01' as date)",
     end_date="cast('2020-01-01' as date)"
    )
--}})
+}}
+)
 select * from date_spine
