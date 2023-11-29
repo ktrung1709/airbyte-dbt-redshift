b44888e802969b64a3d2176e8460d1a24ba0b7dd
ladailam382 <ladailam382@gmail.com>
2023-11-29 23:32:01 +0700

    update

--- macros/date_dim.sql
+++ macros/date_dim.sql
@@ -6,8 +6,6 @@ WITH RECURSIVE RecursiveDates (DateValue) AS (
     FROM RecursiveDates
     WHERE CAST(dateValue as DATE) <= GETDATE()
 )
--- SELECT * FROM RecursiveDates
-
   , 
   dates_fin AS (
   SELECT d1.DateValue AS "Date",
@@ -16,19 +14,12 @@ WITH RECURSIVE RecursiveDates (DateValue) AS (
          cast(extract(year from d1.DateValue) as text) || cast('Q' as text) || cast(EXTRACT(quarter FROM d1.DateValue) as text) AS QuarterID,
          extract(month from d1.DateValue) AS "Month",
          cast(extract(year from d1.DateValue) as text) || cast(EXTRACT(month FROM d1.DateValue) as text) AS MonthID,
-         TO_CHAR(d1.DateValue, 'Month') AS "MonthName",
+         TO_CHAR(d1.DateValue, 'mon') AS "MonthName",
          date_part(dayofweek, d1.DateValue) as DayOfWeek,
          TO_CHAR(d1.DateValue, 'Day') as DayName,
          date_part(day, d1.DateValue) AS "DayOfMonth"
-        --  dbt_date.day_of_month(d1.DateValue) AS DayOfMonth,
-        --  dbt_date.day_name(d1.DateValue) AS DayName
          
   FROM RecursiveDates d1
 )
--- SELECT *,
---        CONCAT ('p',Fin_Period) AS Fin_Period_Name,
---        CONCAT ('FQ',Fin_Quarter) AS Fin_Quarter_Name,
---        CONCAT ('wk',Fin_Week) AS Fin_Week_Name
--- FROM dates_fin
 select * from dates_fin
 {% endmacro %}
