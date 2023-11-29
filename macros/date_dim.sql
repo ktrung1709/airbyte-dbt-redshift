e10b85b298fb376dc91242ae6259a9eecfe892b6
ladailam382 <ladailam382@gmail.com>
2023-11-29 19:55:43 +0700

    update

--- macros/date_dim.sql
+++ macros/date_dim.sql
@@ -2,7 +2,7 @@
 WITH RECURSIVE RecursiveDates (DateValue) AS (
     SELECT CAST('{{ start_date }}' AS DATE) AS DateValue
     UNION ALL
-    SELECT CAST(DATEADD(hour, 12, DateValue) AS DATE) AS DateValue
+    SELECT CAST(DATEADD(day, 1, DateValue) AS DATE) AS DateValue
     FROM RecursiveDates
     WHERE CAST(dateValue as DATE) <= GETDATE()
 )
