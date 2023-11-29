eeb9803acccfa6834dc48e16d8caf8b16a33debf
ladailam382 <ladailam382@gmail.com>
2023-11-29 20:50:12 +0700

    update

--- models/date_time.sql
+++ models/date_time.sql
@@ -1,2 +1,9 @@
 
-{{ generate_dates_dimension ("2015-01-01") }}
+-- {{ generate_dates_dimension ("2015-01-01") }}
+{{
+    dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2019-01-01' as date)",
+    end_date="cast('2020-01-01' as date)"
+   )
+}}
