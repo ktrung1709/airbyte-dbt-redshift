3bea673fd2434c83037929d649289241f00de804
ktrung1709 <ktrung1709@gmail.com>
2023-12-04 06:07:35 +0700

    add incremental airport

--- models/airport_dim.sql
+++ models/airport_dim.sql
@@ -1,9 +1,18 @@
-with airport_source as (
-    select
-        a.airport_id, a.iata, a.icao, a.name, a1.city, a1.country, a1.latitude, a1.longitude
-    from airport a
-    inner join airport_geo a1 on a.airport_id = a1.airport_id
-)
+{{
+    config(
+        materialized='incremental',
+        unique_key='airport_id'
+    )
+}}
 
-select *
-from airport_source
+select
+    a.airport_id, a.iata, a.icao, a.name, a1.city, a1.country, a1.latitude, a1.longitude,
+    getdate() as updated_at
+from airport a
+inner join airport_geo a1 on a.airport_id = a1.airport_id
+
+
+{% if is_incremental() %}
+  where CAST(a."_ab_cdc_updated_at" AS TIMESTAMP) > (select max(updated_at) from {{ this }})
+  or CAST(a1."_ab_cdc_updated_at" AS TIMESTAMP) > (select max(updated_at) from {{ this }})
+{% endif %}
