8e7f6048074b6a007ad9130d4d3a1e55419e6548
ktrung1709 <ktrung1709@gmail.com>
2023-12-04 06:24:39 +0700

    add incremental passenger

--- models/passenger_dim.sql
+++ models/passenger_dim.sql
@@ -1,9 +1,16 @@
-with passenger_source as (
-    SELECT p.passenger_id, p.passportno, p.firstname, p.lastname, pd.birthdate, pd.sex, pd.street, pd.city, pd.zip, pd.country, pd.emailaddress, pd.telephoneno
-    FROM passenger p
-    INNER JOIN passengerdetails pd
-    ON p.passenger_id = pd.passenger_id
-)
+{{
+    config(
+        materialized='incremental',
+        unique_key='passenger_id'
+    )
+}}
 
-select *
-from passenger_source
+SELECT p.passenger_id, p.passportno, p.firstname, p.lastname, pd.birthdate, pd.sex, pd.street, pd.city, pd.zip, pd.country, pd.emailaddress, pd.telephoneno
+FROM passenger p
+INNER JOIN passengerdetails pd
+ON p.passenger_id = pd.passenger_id
+
+{% if is_incremental() %}
+  where CAST(p."_ab_cdc_updated_at" AS TIMESTAMP) > (select max(updated_at) from {{ this }})
+  or CAST(pd."_ab_cdc_updated_at" AS TIMESTAMP) > (select max(updated_at) from {{ this }})
+{% endif %}
