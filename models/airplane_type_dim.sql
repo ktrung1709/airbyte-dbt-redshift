af128207de05d49332e828681ee4f48f7284d316
ktrung1709 <ktrung1709@gmail.com>
2023-12-04 04:24:09 +0700

    add incremental strategy

--- models/airplane_type_dim.sql
+++ models/airplane_type_dim.sql
@@ -1,24 +1,6 @@
--- {{
---     config(
---         materialized='incremental',
---         unique_key='type_id'
---     )
--- }}
-
--- with max_updated_time as 
--- (select max(_ab_cdc_updated_at) from airplane_type)
-
 select
     type_id, 
     identifier,
     "description"
     CURRENT_TIMESTAMP as updated_at
 from airplane_type
-
--- {% if is_incremental() %}
-
---   -- this filter will only be applied on an incremental run
---   -- (uses >= to include records arriving later on the same day as the last run of this model)
---   where _ab_cdc_updated_at >= max_updated_time
-
--- {% endif %}
