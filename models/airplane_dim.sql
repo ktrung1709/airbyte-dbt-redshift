06b6cccd3f9bf80d4238476834277cc085c05399
ktrung1709 <ktrung1709@gmail.com>
2023-12-04 05:56:23 +0700

    add incremental airplane

--- models/airplane_dim.sql
+++ models/airplane_dim.sql
@@ -1,11 +1,18 @@
-with airplane_source as (
-    select
-        airplane_id, 
-        capacity,
-        type_id
-    from airplane
-    where airline_id = 107
-)
+{{
+    config(
+        materialized='incremental',
+        unique_key='airplane_id'
+    )
+}}
 
-select *
-from airplane_source
+select
+    airplane_id, 
+    capacity,
+    type_id,
+    getdate() as updated_at
+from airplane
+where airline_id = 107
+
+{% if is_incremental() %}
+  and CAST("_ab_cdc_updated_at" AS TIMESTAMP) > (select max(updated_at) from {{ this }})
+{% endif %}
--- models/airplane_type_dim.sql
+++ models/airplane_type_dim.sql
@@ -13,9 +13,5 @@ select
 from airplane_type
 
 {% if is_incremental() %}
-
-  -- this filter will only be applied on an incremental run
-  -- (uses > to include records whose timestamp occurred since the last run of this model)
   where CAST("_ab_cdc_updated_at" AS TIMESTAMP) > (select max(updated_at) from {{ this }})
-
 {% endif %}
