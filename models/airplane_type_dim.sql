134052d902b9fb8b2ea19f17c42a1b405c453211
ktrung1709 <ktrung1709@gmail.com>
2023-12-04 03:52:37 +0700

    add incremental strategy

--- models/airplane_type_dim.sql
+++ models/airplane_type_dim.sql
@@ -1,10 +1,19 @@
-with airplane_type_source as (
-    select
-        type_id, 
-        identifier,
-        "description"
-    from airplane_type
-)
+{{
+    config(
+        materialized='incremental',
+        unique_key='type_id'
+    )
+}}
+select
+    type_id, 
+    identifier,
+    "description"
+from airplane_type
 
-select *
-from airplane_type_source
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records arriving later on the same day as the last run of this model)
+  where _ab_cdc_updated_at >= (select max(_ab_cdc_updated_at) from {{ this }})
+
+{% endif %}
