c719e9df7c9e4bbb155bc970ea4e933a411658fa
ladailam382 <ladailam382@gmail.com>
2023-12-12 14:22:28 +0700

    update

---
+++ macros/distance_calculation.sql
@@ -0,0 +1,8 @@
+{% macro haversine(lat1, lon1, lat2, lon2) %}
+    lat1 + lon1 + lat2 + lon2
+    -- 6371 * 2 * ASIN(SQRT(
+    --     POWER(SIN(({{ lat2 }} - abs({{ lat1 }})) * pi()/180 / 2), 2) +
+    --     COS({{ lat1 }} * pi()/180) * COS(abs({{ lat2 }}) * pi()/180) *
+    --     POWER(SIN(({{ lon2 }} - {{ lon1 }}) * pi()/180 / 2), 2)
+    -- ))
+{% endmacro %}
---
+++ macros/get_coordinates.sql
@@ -0,0 +1,18 @@
+{% macro get_latlong(airport_id) %}
+
+{% set query %}
+    select latitude, longitude
+    from airport_geo
+    where id = {{airport_id}}
+
+{% endset %}
+
+
+{% if execute %}
+{% set lat = run_query(query).columns[0][0] %}
+{% else %}
+{% set lat = "" %}
+{% endif %}
+{% do return(lat) %}
+
+{% endmacro %}
