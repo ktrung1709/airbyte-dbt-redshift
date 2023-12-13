06120c797e5e5ca10f0e2bb7de584d6d5fc82b28
ladailam382 <ladailam382@gmail.com>
2023-12-13 23:36:25 +0700

    update

--- macros/distance_calculation.sql
+++ macros/distance_calculation.sql
@@ -1,7 +1,7 @@
 {% macro haversine(lat1, lon1, lat2, lon2) %} 
-     ASIN(SQRT(
-        POWER(SIN(({{ lat2 }} - abs({{ lat1 }})) * pi()/180 / 2), 2) +
-        COS({{ lat1 }} * pi()/180) * COS(abs({{ lat2 }}) * pi()/180) *
-        POWER(SIN(({{ lon2 }} - {{ lon1 }}) * pi()/180 / 2), 2)
-    ))
+    6371 * 2 * ASIN(SQRT(
+    POWER(SIN(({{ lat2 }} - {{ lat1 }}) * pi()/180 / 2), 2) +
+    COS({{ lat1 }} * pi()/180) * COS({{ lat2 }} * pi()/180) *
+    POWER(SIN(({{ lon2 }} - {{ lon1 }}) * pi()/180 / 2), 2)
+  ))
 {% endmacro %}
